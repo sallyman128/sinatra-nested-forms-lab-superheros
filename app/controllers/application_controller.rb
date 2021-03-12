da31fd993db411e02f216d9d5288ab6050840bae
Salmaan <salmaan.arif.ali@gmail.com>
2021-03-11 21:18:51 -0500

    commit tree 8191d0c711236a6808520a23a9be372520ab3dba

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :"../views/super_hero"
+    end
 
+    post '/teams' do
+        @team = Team.new(params[:team])
+
+        Hero.clear
+
+        params[:team][:members].each do |hero|
+            Hero.new(hero)
+        end
+
+        @heros = Hero.all
+
+        erb :"../views/team"
+    end
 end
